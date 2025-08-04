55aa15a2011c0a4ab7aaf1fa0bd2409a97b73d2e
Guilherme Vitor Neves Pereira <vitorguilh@hotmail.com>
2025-08-04 14:13:44 -0300

    iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (MinimalApi.Dominio.DTOs.LoginDTO loginDto) =>
+{
+if (loginDto.Email == "adm@teste.com" && loginDto.Senha == "123456")
+    return Results.Ok("Login com sucesso!");
+else
+    return Results.Unauthorized();    
+});
+
+app.Run();
+
